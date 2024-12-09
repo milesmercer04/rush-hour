1f307679c3f5939feffc2c07154711602592f0a4
milesmercer04 <milesmercer04@gmail.com>
2024-12-09 15:56:29 -0800

    Completed bidirectional heuristic solver implementation, passing all but two unit tests

--- rush-hour-solver/src/main/java/com/rushhour/BidirectionalHeuristicSolver.java
+++ rush-hour-solver/src/main/java/com/rushhour/BidirectionalHeuristicSolver.java
@@ -1,10 +1,13 @@
 package com.rushhour;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class BidirectionalHeuristicSolver implements Solver {
   Board initialState;
@@ -73,7 +76,7 @@ public class BidirectionalHeuristicSolver implements Solver {
     // Set of states reachable from initial state
     HashSet<Board> reachableFromStart = new HashSet<>();
     int numberOfCars = initialState.numberOfCars();
-    Board newState;
+    Board newState = null;
     for (int i = 0; i < numberOfCars; i++) {
       newState = initialState.tryMove(i, true);
       if (newState != null && !visitedFromStart.contains(newState)) {
@@ -112,7 +115,165 @@ public class BidirectionalHeuristicSolver implements Solver {
       }
     }
 
-    return new ArrayList<>();
+    // Comparator of reachable board states in forward direction
+    Comparator<Board> forwardBoardComparator = (Board a, Board b) -> {
+      if (a.equals(b)) {
+        return 0;
+      }
+      Integer distanceA = distanceFromStart.get(a);
+      Integer distanceB = distanceFromStart.get(b);
+      Integer heuristicA = forwardHeuristic.get(a);
+      if (heuristicA == null) {
+        heuristicA = a.minimumDistanceFrom(winStates);
+        forwardHeuristic.put(a, heuristicA);
+      }
+      Integer heuristicB = forwardHeuristic.get(b);
+      if (heuristicB == null) {
+        heuristicB = b.minimumDistanceFrom(winStates);
+        forwardHeuristic.put(b, heuristicB);
+      }
+      if (distanceA == null) {
+        if (distanceB == null) {
+          return 0;
+        } else {
+          return 1;
+        }
+      } else {
+        if (distanceB == null) {
+          return -1;
+        }
+      }
+      return Integer.compare(distanceA + heuristicA, distanceB + heuristicB);
+    };
+
+    // Comparator of reachable board states in backward direction
+    Comparator<Board> backwardBoardComparator = (Board a, Board b) -> {
+      if (a.equals(b)) {
+        return 0;
+      }
+      Integer distanceA = distanceFromWinState.get(a);
+      Integer distanceB = distanceFromWinState.get(b);
+      Integer heuristicA = backwardHeuristic.get(a);
+      if (heuristicA == null) {
+        heuristicA = a.distanceFrom(initialState);
+      }
+      Integer heuristicB = backwardHeuristic.get(b);
+      if (heuristicB == null) {
+        heuristicB = b.distanceFrom(initialState);
+      }
+      if (distanceA == null) {
+        if (distanceB == null) {
+          return 0;
+        } else {
+          return 1;
+        }
+      } else {
+        if (distanceB == null) {
+          return -1;
+        }
+      }
+      return Integer.compare(distanceA + heuristicA, distanceB + heuristicB);
+    };
+
+    // Put all states reachable from initial state into a priority queue based on forward comparator
+    PriorityQueue<Board> forwardReachableQueue = new PriorityQueue<>(forwardBoardComparator);
+    for (Board b : reachableFromStart) {
+      forwardReachableQueue.add(b);
+    }
+
+    // Put all states reachable from a win state into a priority queue based on backward comparator
+    PriorityQueue<Board> backwardReachableQueue = new PriorityQueue<>(backwardBoardComparator);
+    for (Board b : reachableFromWinState) {
+      backwardReachableQueue.add(b);
+    }
+
+    // Explore new board states until a state has been visited in both directions or one side runs
+    // out of options
+    boolean bridgeFound = false;
+    Board reachableFromNewState;
+    while (forwardReachableQueue.peek() != null && backwardReachableQueue.peek() != null) {
+      // Work from side with less reachable states
+      if (reachableFromStart.size() < reachableFromWinState.size()) {
+        // Searching in forward direction
+        newState = forwardReachableQueue.poll();
+        visitedFromStart.add(newState);
+        reachableFromStart.remove(newState);
+        // Check if the new state has also been visited from a win state
+        if (visitedFromWinState.contains(newState)) {
+          bridgeFound = true;
+          break;
+        }
+        // Find all new states reachable from the added state
+        for (int i = 0; i < numberOfCars; i++) {
+          reachableFromNewState = newState.tryMove(i, true);
+          if (reachableFromNewState != null && !visitedFromStart.contains(reachableFromNewState) &&
+              !reachableFromStart.contains(reachableFromNewState)) {
+            reachableFromStart.add(reachableFromNewState);
+            distanceFromStart.put(reachableFromNewState, distanceFromStart.get(newState) + 1);
+            forwardHeuristic.put(reachableFromNewState, reachableFromNewState.minimumDistanceFrom(winStates));
+            parentFromStart.put(reachableFromNewState, newState);
+            forwardReachableQueue.add(reachableFromNewState);
+          }
+          reachableFromNewState = newState.tryMove(i, false);
+          if (reachableFromNewState != null && !visitedFromStart.contains(reachableFromNewState) &&
+              !reachableFromStart.contains(reachableFromNewState)) {
+            reachableFromStart.add(reachableFromNewState);
+            distanceFromStart.put(reachableFromNewState, distanceFromStart.get(newState) + 1);
+            forwardHeuristic.put(reachableFromNewState, reachableFromNewState.minimumDistanceFrom(winStates));
+            parentFromStart.put(reachableFromNewState, newState);
+            forwardReachableQueue.add(reachableFromNewState);
+          }
+        }
+      } else {
+        // Searching in backward direction
+        newState = backwardReachableQueue.poll();
+        visitedFromWinState.add(newState);
+        reachableFromWinState.remove(newState);
+        // Check if the new state has also been visited from the initial state
+        if (visitedFromStart.contains(newState)) {
+          bridgeFound = true;
+          break;
+        }
+        // Find all new states reachable from the added state
+        for (int i = 0; i < numberOfCars; i++) {
+          reachableFromNewState = newState.tryMove(i, true);
+          if (reachableFromNewState != null && !visitedFromWinState.contains(reachableFromNewState) &&
+              !reachableFromWinState.contains(reachableFromNewState)) {
+            reachableFromWinState.add(reachableFromNewState);
+            distanceFromWinState.put(reachableFromNewState, distanceFromWinState.get(newState) + 1);
+            backwardHeuristic.put(reachableFromNewState, reachableFromNewState.distanceFrom(initialState));
+            parentFromWinState.put(reachableFromNewState, newState);
+            backwardReachableQueue.add(reachableFromNewState);
+          }
+          reachableFromNewState = newState.tryMove(i, false);
+          if (reachableFromNewState != null && !visitedFromWinState.contains(reachableFromNewState) &&
+              !reachableFromWinState.contains(reachableFromNewState)) {
+            reachableFromWinState.add(reachableFromNewState);
+            distanceFromWinState.put(reachableFromNewState, distanceFromWinState.get(newState) + 1);
+            backwardHeuristic.put(reachableFromNewState, reachableFromNewState.distanceFrom(initialState));
+            parentFromWinState.put(reachableFromNewState, newState);
+            backwardReachableQueue.add(reachableFromNewState);
+          }
+        }
+      }
+    }
+
+    // If no bridge was found between the forward and backward searches, no solution exists
+    if (!bridgeFound) {
+      return new LinkedList<>();
+    }
+
+    // Build the solution from the two search trees
+    LinkedList<Board> solution = new LinkedList<>();
+    Board currentState;
+    solution.add(newState);
+    while ((currentState = parentFromStart.get(solution.getFirst())) != null) {
+      solution.addFirst(currentState);
+    }
+    while ((currentState = parentFromWinState.get(solution.getLast())) != null) {
+      solution.addLast(currentState);
+    }
+    return solution;
   }
 
   public HashSet<Board> feasibleWinStates() {
